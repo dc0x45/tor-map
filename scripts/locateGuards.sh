eb0a3d43c6deea395207c173a2c1815544b9818f
dcote007 <32779137+dcote007@users.noreply.github.com>
2020-09-12 12:26:37 -0400

    Add files via upload

---
+++ scripts/locateGuards.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#This script outputs to CSV format, use the command "bash locateExits.sh | tee exitData.csv" to both see the output in stdout and write to the CSV file
+
+rawList=$(curl -s "https://onionoo.torproject.org/details?search=flag:Guard%20running:true&fields=or_addresses")
+
+formList=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' <<< $rawList)
+
+echo "Lat,Long,ISP,Hosting,IP"
+
+for ip in $formList
+do
+	data=$(curl -s "http://ip-api.com/csv/$ip?fields=16786112")
+	echo "$data"
+	sleep 1.4s
+done
